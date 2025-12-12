672ccdd915bf1cc6fc05aebc826b0786109b1d08
hkndgn805-afk <hkndgn805@gmail.com>
2025-12-13 02:06:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,18 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 app = Flask(__name__)
 
+# START KOMUTU
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "🤖 MDH Trade Bot çalışıyor!")
+
+# TÜM MESAJLAR
+@bot.message_handler(func=lambda message: True)
+def all_messages(message):
+    bot.reply_to(
+        message,
+        "🤖 MDH Trade Bot çalışıyor!\n\nKomutlar:\n/start\n/btc"
+    )
 
 def run_bot():
     bot.infinity_polling()
@@ -22,9 +31,3 @@ def home():
 if __name__ == "__main__":
     threading.Thread(target=run_bot).start()
     app.run(host="0.0.0.0", port=10000)
-@bot.message_handler(func=lambda message: True)
-def all_messages(message):
-    bot.reply_to(
-        message,
-        "🤖 MDH Trade Bot çalışıyor!\n\nKomutlar:\n/start\n/btc"
-    )
