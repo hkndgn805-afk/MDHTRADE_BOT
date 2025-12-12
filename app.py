fa4e58b4d34fc801c9b11c475f5a33a8f7ca221b
hkndgn805-afk <hkndgn805@gmail.com>
2025-12-13 01:54:18 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import telebot
+import os
+import threading
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+app = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "🤖 MDH Trade Bot çalışıyor!")
+
+def run_bot():
+    bot.infinity_polling()
+
+@app.route("/")
+def home():
+    return "Bot aktif 🚀"
+
+if __name__ == "__main__":
+    threading.Thread(target=run_bot).start()
+    app.run(host="0.0.0.0", port=10000)
